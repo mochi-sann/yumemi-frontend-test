8c574aec69bf9a4f4d7fbb6a2a526dfeda6da855
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2024-11-03 10:45:17 +0900

    fix: update tailwind css

--- src/Components/ui/CheckBox.tsx
+++ src/Components/ui/CheckBox.tsx
@@ -3,14 +3,23 @@ import type React from "react";
 export type CheckBoxProps = {
 	name: string;
 	prefCode: string;
+	CheckBoxProps?: React.DetailedHTMLProps<
+		React.InputHTMLAttributes<HTMLInputElement>,
+		HTMLInputElement
+	>;
 };
 
 export const CheckBox: React.FC<CheckBoxProps> = (props) => {
 	return (
 		<div>
-			<label className="flex items-center w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+			<label className="inline-flex items-center ">
 				{props.name}
-				<input type="checkbox" value={props.prefCode} />
+				<input
+					className="w-4 h-4  border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+					type="checkbox"
+					value={props.prefCode}
+					{...props.CheckBoxProps}
+				/>
 			</label>
 		</div>
 	);
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import "./index.css";
+
 import { queryClient } from "./lib/api/FetchClient.ts";
 
 createRoot(document.getElementById("root") as HTMLElement).render(

9b8d4bbb44096a130e59a10d34b46e69112c5774
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2024-11-04 22:15:33 +0900

    chore: グラフコンポーネントを更新

--- src/Components/ui/PopulationGraph/PopulationGraph.stories.ts
+++ src/Components/ui/PopulationGraph/PopulationGraph.stories.ts
@@ -23,5 +23,104 @@ type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
-	args: {},
+	args: {
+		chartTitle: "ちゃーとたいとる",
+		PrefChart: [
+			{
+				PrefName: "北海道",
+				data: [
+					{
+						year: 1960,
+						value: 1681479,
+						rate: 33.37,
+					},
+					{
+						year: 1965,
+						value: 1462123,
+						rate: 28.27,
+					},
+					{
+						year: 1970,
+						value: 1309487,
+						rate: 25.26,
+					},
+					{
+						year: 1975,
+						value: 1312611,
+						rate: 24.59,
+					},
+					{
+						year: 1980,
+						value: 1298324,
+						rate: 23.28,
+					},
+					{
+						year: 1985,
+						value: 1217959,
+						rate: 21.45,
+					},
+					{
+						year: 1990,
+						value: 1034251,
+						rate: 18.33,
+					},
+					{
+						year: 1995,
+						value: 898673,
+						rate: 15.79,
+					},
+					{
+						year: 2000,
+						value: 792352,
+						rate: 13.94,
+					},
+					{
+						year: 2005,
+						value: 719057,
+						rate: 12.78,
+					},
+					{
+						year: 2010,
+						value: 657312,
+						rate: 11.94,
+					},
+					{
+						year: 2015,
+						value: 608296,
+						rate: 11.3,
+					},
+					{
+						year: 2020,
+						value: 555804,
+						rate: 10.64,
+					},
+					{
+						year: 2025,
+						value: 511677,
+						rate: 10.2,
+					},
+					{
+						year: 2030,
+						value: 465307,
+						rate: 9.71,
+					},
+					{
+						year: 2035,
+						value: 423382,
+						rate: 9.31,
+					},
+					{
+						year: 2040,
+						value: 391086,
+						rate: 9.14,
+					},
+					{
+						year: 2045,
+						value: 360177,
+						rate: 8.99,
+					},
+				],
+			},
+		],
+	},
 };
--- src/Components/ui/PopulationGraph/PopulationGraph.tsx
+++ src/Components/ui/PopulationGraph/PopulationGraph.tsx
@@ -1,29 +1,63 @@
 import type React from "react";
 
-export type PopulationGraphProps = {};
-
 import * as Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 import { useRef } from "react";
 
+export type PopulationGraphProps = {
+	chartTitle: string;
+	PrefChart: Array<{
+		PrefName: String;
+		label?: string;
+		data: Array<{
+			year: number;
+			value: number;
+			rate?: number;
+		}>;
+	}>;
+};
 // The wrapper exports only a default component that at the same time is a
 // namespace for the related Props interface (HighchartsReact.Props) and
 // RefObject interface (HighchartsReact.RefObject). All other interfaces
 // like Options come from the Highcharts module itself.
 
-const options: Highcharts.Options = {
-	title: {
-		text: "My chart",
-	},
-	series: [
-		{
-			type: "line",
-			data: [1, 2, 3],
-		},
-	],
-};
-
 export const PopulationGraph: React.FC<PopulationGraphProps> = (props) => {
+	const options: Highcharts.Options = {
+		title: {
+			text: props.chartTitle,
+		},
+		chart: {
+			type: "spline",
+		},
+
+		plotOptions: {
+			series: {
+				label: {
+					connectorAllowed: false,
+				},
+
+				pointStart: 1960,
+				pointInterval: 5,
+			},
+		},
+
+		yAxis: {
+			title: {
+				text: "人口",
+			},
+			labels: {
+				format: "{value}人",
+			},
+		},
+		tooltip: {
+			shared: false,
+		},
+
+		series: props.PrefChart.map((pref) => ({
+			name: `${pref.PrefName} ${pref.label ? `: ${pref.label}` : ""}`,
+			data: pref.data.map((value) => value.value),
+		})) as Highcharts.SeriesOptionsType[],
+	};
 	const chartComponentRef = useRef<HighchartsReact.RefObject>(null);
 	return (
 		<HighchartsReact

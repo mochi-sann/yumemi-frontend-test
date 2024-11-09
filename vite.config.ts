1b5a956dde74e8a54e8e8133b8b2649b686d2a22
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2024-11-09 22:41:18 +0900

    feat: analyzerを追加した

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import { analyzer } from "vite-bundle-analyzer";
 import { configDefaults } from "vitest/config";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [TanStackRouterVite(), react()],
+	plugins: [TanStackRouterVite(), react(), analyzer()],
 	test: {
 		environment: "jsdom", // Vitestでjsdomをテスト環境として使用
 		globals: true,
